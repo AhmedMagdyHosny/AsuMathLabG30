fe3b3eb5b187d3581cc76a1216b8b86cc2ed0f27
Mohamed Elattar <30553162+mohamedel3attar@users.noreply.github.com>
2018-01-30 00:11:50 +0200

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -9,6 +9,9 @@
 #include <string>
 #include <stdlib.h>
 #include <string.h>
+#include <ctime>
+#include <vector>
+#include <math.h>
 using namespace std;
 
 
@@ -17,17 +20,24 @@ class CMatrix {
 	double ** values ;
 	string name;
 
+	static bool isNumber(string s);
+
 public :
-	// constructors and destructors
+	enum MI { MI_ZEROS, MI_ONES, MI_EYE, MI_RAND, MI_VALUE };
+	
+	//=================================
+	//Constructors and destructors
 	CMatrix () ;
 	CMatrix (int nR , int nC) ;
-	void reset ();
-	~CMatrix () ;
+	CMatrix(int nR, int nC, int initialization, double initializationValue=0);
+	CMatrix(const CMatrix& m);
 	CMatrix(string s);
 	CMatrix(double d);
-	CMatrix(const CMatrix& m);
-	CMatrix(int nR, int nC, int initialization, double initializationValue=0);
-	//functions
+	void reset ();
+	~CMatrix () ;
+	
+	//=================================
+	//Functions
 	void copy(const CMatrix& m);
 	void copy(double d);
 	void copy(string s);
@@ -37,44 +47,54 @@ public :
 	void add(const CMatrix& m);
 	void sub(const CMatrix& m);
 	void mul(const CMatrix& m);
+	void dotSlash(int num);
+	CMatrix power(double p);
+	static string MatrixCat(string s);
+	static CMatrix trigStrtoDouble(string trigExpression ,vector<CMatrix> &matrix_List);
+	static CMatrix matrixInit(string expression);
 	void set_name(string n);
 	string get_name();
-	void PrintMatrix();
-	string getString();
 	string get_string();
-	CMatrix matrixInit(string expression);
-	
+	string getString();
+	void PrintMatrix();
+	int get_nR();
+	int get_nC();
 	void transpose(const CMatrix &m);
-	void dotSlash(int num);
 	CMatrix getCofactor(int r, int c);
 	double getDeterminant();
 	CMatrix getInverse();
 	CMatrix getAdjoint(const CMatrix &m);
-	static string MatrixCat(string s);
-        CMatrix power(double p);
-
-
+	//=================================
+	// Trigonometric functions (sin ,cos ,tan ,sinh ,cosh ,tanh ,sin^-1 ,cos^-1 ,tan^-1 ,sinh^-1 ,cosh^-1 ,tanh^-1);
+    static CMatrix matsin(CMatrix &M);  //sin
+    static CMatrix matcos(CMatrix &M);  //cos
+    static CMatrix mattan(CMatrix &M);  //tan
+    static CMatrix matsinInv(CMatrix &M);  //asin2
+    static CMatrix matcosInv(CMatrix &M);  //acos2
+    static CMatrix mattanInv(CMatrix &M);  //atan2
+    static CMatrix matcosh(CMatrix &M);    //cosh
+    static CMatrix matsinh(CMatrix &M);    //sinh
+    static CMatrix mattanh(CMatrix &M);    //tanh
+    /*static CMatrix matcoshInv(CMatrix &M);   //acosh
+    static CMatrix matsinhInv(CMatrix &M);   //asinh
+    static CMatrix mattanhInv(CMatrix &M);   //atanh*/
+	
+	//=================================
 	//operators
 	CMatrix operator=(const CMatrix& m);
 	CMatrix operator=(double d);
 	void operator+=(const CMatrix& m);
 	CMatrix operator+(const CMatrix& m);
-	void operator-=(const CMatrix& m);
+	CMatrix operator+(double d);
+	void operator+=(double d);
 	CMatrix operator-(const CMatrix& m);
-	void operator*=(const CMatrix& m);
+	void operator-=(const CMatrix& m);
+	CMatrix operator-(double d);
+	void operator-=(double d);
 	CMatrix operator*(const CMatrix& m);
+	void operator*=(const CMatrix& m);
 	CMatrix operator/(const CMatrix& m);
 	void operator/=(const CMatrix& m);
-	
-	//Trig Functions
-	CMatrix matsin(CMatrix &M);
-	CMatrix matcos(CMatrix &M);
-	CMatrix mattan(CMatrix &M);
-	
-	
-	//String Trig Expression To Double Value..
-	CMatrix trigStrtoDouble(string trigExpression ,vector<CMatrix> &matrix_List);
-
 };
 
 #endif
